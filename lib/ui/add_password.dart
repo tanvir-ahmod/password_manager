fb6448ffcb5c598f8508437ba7c07ebb58345306
tanvir-ahmod <tanvirahmodcse@gmail.com>
2021-04-12 13:26:44 +0600

    Add password add UI

---
+++ lib/ui/add_password.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddPassword extends StatefulWidget {
+  @override
+  _AddPasswordState createState() => _AddPasswordState();
+}
+
+class _AddPasswordState extends State<AddPassword> {
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _userName = TextEditingController();
+  final TextEditingController _pass = TextEditingController();
+
+  bool _isPasswordHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final node = FocusScope.of(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Add password")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Image.asset(
+                "assets/images/password_setup.png",
+                height: size.height * 0.3,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Form(
+                key: _form,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _title,
+                      textInputAction: TextInputAction.next,
+                      validator: (val) {
+                        if (val.isEmpty) return 'Field can not be empty';
+                        return null;
+                      },
+                      onEditingComplete: () => node.nextFocus(),
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _userName,
+                      textInputAction: TextInputAction.next,
+                      onEditingComplete: () => node.nextFocus(),
+                      decoration: InputDecoration(
+                        labelText: 'Username / Email (optional)',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _pass,
+                      textInputAction: TextInputAction.done,
+                      obscureText: _isPasswordHidden,
+                      validator: (val) {
+                        if (val.isEmpty) return 'Field can not be empty';
+                        return null;
+                      },
+                      onEditingComplete: () => node.unfocus(),
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        suffix: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _isPasswordHidden = !_isPasswordHidden;
+                            });
+                          },
+                          child: Icon(
+                            _isPasswordHidden
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text("Save", style: TextStyle(fontSize: 16),),
+              style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.lightBlue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.lightBlue)))),
+              onPressed: () async {
+                if (_form.currentState.validate()) {}
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:password_manager/ui/add_password.dart';
 import 'package:password_manager/ui/enter_master_password.dart';
 import 'package:password_manager/ui/home.dart';
 import 'package:password_manager/ui/setup_master_password.dart';
@@ -6,6 +7,7 @@ import 'package:password_manager/ui/setup_master_password.dart';
 const String HomeRoute = "/";
 const String SetupMasterPasswordRoute = "/setupMasterPassword";
 const String EnterMasterPasswordRoute = "/enterMasterPassword";
+const String AddPasswordRoute = "/addMasterPassword";
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -16,6 +18,8 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => SetupMasterPassword());
       case EnterMasterPasswordRoute:
         return MaterialPageRoute(builder: (_) => EnterMasterPassword());
+      case AddPasswordRoute:
+        return MaterialPageRoute(builder: (_) => AddPassword());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
