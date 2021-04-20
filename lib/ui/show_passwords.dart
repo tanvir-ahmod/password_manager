ef399e786329543598ee170545eecf570e9c908e
tanvir-ahmod <tanvirahmodcse@bs-23.com>
2021-04-20 17:34:08 +0600

    Implement dragging password UI

--- lib/ui/show_passwords.dart
+++ lib/ui/show_passwords.dart
@@ -11,10 +11,22 @@ import 'package:password_manager/utils/app_router.dart';
 const String CHANGE_MASTER_PASSWORD = "Change Master Password";
 const String LOGOUT = "Logout";
 
-class ShowPasswords extends StatelessWidget {
+class ShowPasswords extends StatefulWidget {
+  @override
+  _ShowPasswordsState createState() => _ShowPasswordsState();
+}
+
+class _ShowPasswordsState extends State<ShowPasswords> {
+  var _isDraggable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<ShowPasswordBloc>(context).add(GetPasswordsEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
-    BlocProvider.of<ShowPasswordBloc>(context).add(GetPasswordsEvent());
     return BlocBuilder<ShowPasswordBloc, ShowPasswordState>(
         builder: (context, state) {
       if (state is GetPasswordState) {
@@ -26,52 +38,56 @@ class ShowPasswords extends StatelessWidget {
   }
 
   Widget _showPasswords(BuildContext context, List<PasswordModel> passwords) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final evenItemColor = colorScheme.primary.withOpacity(0.15);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Show passwords"),
-        actions: <Widget>[
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              _handleMenuClick(context, value);
-            },
-            itemBuilder: (BuildContext context) {
-              return {CHANGE_MASTER_PASSWORD, LOGOUT}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
+      appBar: _showAppBar(),
       body: () {
         if (passwords.isEmpty) {
           return Center(
             child: Text("No Data Found"),
           );
-        } else
-          return ListView.separated(
-              itemCount: passwords.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(passwords[index].title),
-                  subtitle: Text(passwords[index].userName),
-                  leading: CircleAvatar(
-                    backgroundImage:
-                        AssetImage('assets/images/lock_icon_2.png'),
+        } else {
+          return ReorderableListView(
+            buildDefaultDragHandles: true,
+            children: <Widget>[
+              for (int index = 0; index < passwords.length; index++)
+                Container(
+                  key: Key('$index'),
+                  color: index.isOdd ? oddItemColor : evenItemColor,
+                  child: ListTile(
+                    title: Text(passwords[index].title),
+                    subtitle: Text(passwords[index].userName),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/lock_icon_2.png'),
+                    ),
+                    trailing: () {
+                      return _isDraggable
+                          ? Icon(Icons.drag_handle)
+                          : Icon(Icons.keyboard_arrow_right);
+                    }(),
+                    onTap: () {
+                      Navigator.pushNamed(context, ShowDetailsRoute,
+                          arguments:
+                              PasswordModelWithIndex(index, passwords[index]));
+                    },
                   ),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-                    Navigator.pushNamed(context, ShowDetailsRoute,
-                        arguments:
-                            PasswordModelWithIndex(index, passwords[index]));
-                  },
-                );
-              },
-              separatorBuilder: (context, index) {
-                return Divider();
+                ),
+            ],
+            onReorder: (oldIndex, newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final item = passwords.removeAt(oldIndex);
+                passwords.insert(newIndex, item);
               });
+            },
+          );
+        }
       }(),
       floatingActionButton: FloatingActionButton(
         // isExtended: true,
@@ -83,6 +99,79 @@ class ShowPasswords extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  Widget _showAppBar() {
+    return AppBar(
+      title: Text("Show passwords"),
+      actions: <Widget>[
+        () {
+          if (_isDraggable)
+            return Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isDraggable = false;
+                        });
+                      },
+                      child: Icon(
+                        Icons.done,
+                        size: 26.0,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isDraggable = false;
+                        });
+                      },
+                      child: Icon(
+                        Icons.close,
+                        size: 26.0,
+                      ),
+                    )),
+              ],
+            );
+          else
+            return Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isDraggable = true;
+                        });
+                      },
+                      child: Icon(
+                        Icons.drag_indicator,
+                        size: 26.0,
+                      ),
+                    )),
+                PopupMenuButton<String>(
+                  onSelected: (value) {
+                    _handleMenuClick(context, value);
+                  },
+                  itemBuilder: (BuildContext context) {
+                    return {CHANGE_MASTER_PASSWORD, LOGOUT}
+                        .map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            );
+        }()
+      ],
     );
   }
 
